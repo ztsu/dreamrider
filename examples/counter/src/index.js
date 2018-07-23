33f03d61ed8ac523f9f9dbed6da2f27fca6e0061
Alexey Golovnya <alexey.golovnya@gmail.com>
2018-07-23 22:44:47 +0700

    All-new (pre-1)

--- examples/counter/src/index.js
+++ examples/counter/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import {init} from "../../../src/state";
 import {connect} from "../../../src/react";
 
-const inc = ({state}) => ({state: {...state, counter: state.counter + 1}})
+const inc = ({state}) => ({...state, counter: state.counter + 1})
 
 const inc2 = ({and}) => and(inc, inc)
 
-const dec = ({state}) => ({state: {...state, counter: state.counter - 1}})
+const dec = ({state}) => ({...state, counter: state.counter - 1})
 
 const incAsync = ({apply}) => () => setTimeout(() => apply(inc), 1000)
 
@@ -15,9 +15,9 @@ const inc2Async = ({apply}) => () => apply(incAsync, incAsync)
 
 const decAsync = ({apply}) => () => setTimeout(() => apply(dec), 1000)
 
-const startLoading = ({state}) => ({state: {...state, loading: state.loading + 1}})
+const startLoading = ({state}) => ({...state, loading: state.loading + 1})
 
-const stopLoading = ({state}) => ({state: {...state, loading: state.loading - 1}})
+const stopLoading = ({state}) => ({...state, loading: state.loading - 1})
 
 const processInc = ({apply}) => () => apply(startLoading, incAsync, stopLoading)
 
@@ -51,3 +51,29 @@ ReactDOM.render(
 	),
 	document.getElementById("root")
 )
+
+subscribe((state) => console.log("State:", state))
+
+const first = ({state, error}) => { console.log("first", state, error); return {...state, counter: state.counter + 1} }
+const asyncFirst = () => ({apply}) => {setTimeout(() => apply(first), 1000)}
+const second = ({state, error}) => { console.log("second", state, error); return {...state, counter: state.counter + 1} }
+const asyncSecond = () => ({apply}) => {setTimeout(() => apply(second), 1000)}
+const third = ({state, error}) => { console.log("third", state, error); return {...state, counter: state.counter + 1} }
+const fourth = ({state, error}) => { console.log("fourth", state, error); return {...state, counter: state.counter + 1} }
+const fail = ({state, error}) => { console.log("fail", state, error); return new Error("Fail!") }
+const delayedFail = () => ({apply}) => { setTimeout(() => apply(fail), 1000) }
+const recover = ({state, error}) => { console.error("recover", state, error); return state }
+const delayedWait = () => ({apply})=> { setTimeout(() => apply(wait), 1000) }
+const wait = ({state}) => { console.log("wait"); return state }
+
+const test1 = first
+const test2 = asyncFirst
+const test3 = ({and}) => and(first, second, third)
+const test4 = ({and}) => and(asyncFirst, second, third)
+const test5 = ({and}) => and(first, asyncSecond)
+const test6 = ({and}) => and(asyncFirst, asyncSecond, third)
+const test7 = ({and}) => and(first, fail, second) // !
+const test8 = ({and}) => and(fail, second) // !
+const test9 = ({and}) => and(fail)
+
+apply(test9)
--- src/state.js
+++ src/state.js
@@ -1,64 +1,43 @@
-export function and(update, state, react, {error}) {
+export function and(update, state, react, payload) {
 	return (...actions) => {
 		if (actions.length > 0) {
 			const action = actions.shift()
 			let result = {}
 
+			if (typeof action !== "function") {
+				throw new TypeError(action + " is not a function")
+			}
+
 			const apply = (...args) => {
 				if (args.length + actions.length > 0) {
-					and(update, state, react, {error})(...args, ...actions)
+					and(update, state, react, payload)(...args, ...actions)
 				}
 			}
 
-			if (typeof action == "function") {
-				result = action({
-					state: state(),
-					apply,
-					and: (...args) => () => apply(...args),
-					error
-				})
+			result = action({
+				state: state(),
+				and: (...args) => () => apply(...args),
+				error: payload.error
+			})
 
-				if (typeof result == "function") {
-					result()
-					return
-				}
+			if (typeof result == "function") {
+				result({apply})
+				return
+			}
 
-				update(result.state)
+			if (result instanceof Error === false) {
+				update(result)
 				react(state())
 			}
 
 			if (actions.length > 0) {
-				and(update, state, react, {error: result.error})(...actions)
-			}
-		}
-	}
-}
+				const payload = {}
 
-export function or(update, state, react) {
-	return (...actions) => {
-		if (actions.length > 0) {
-			const action = actions.shift()
-			let result = {}
-
-			if (typeof action == "function") {
-				result = action({state: state()})
-
-				if (typeof result == "function") {
-					result((...args) => {
-						if (args.length + actions.length > 0) {
-							and(update, state, react, {})(...args, ...actions)
-						}
-					})
-					return
+				if (result instanceof Error) {
+					payload.error = result
 				}
 
-				if (result.error === undefined) {
-					update(result.state)
-					react(state())
-
-				} else if (actions.length > 0) {
-					or(update, state, react, {error: result.error})(...actions)
-				}
+				return and(update, state, react, payload)(...actions)
 			}
 		}
 	}
@@ -70,7 +49,6 @@ export function init(value) {
 
 	return {
 		apply: and(newState => state = newState, () => state, state => subscribers.forEach(fn => fn(state)), {}),
-		applyOr: (...actions) => () => or(newState => state = newState, () => state, state => subscribers.forEach(f => f(state)))(...actions),
 		subscribe: f => subscribers.push(f),
 		getState: () => state
 	}
