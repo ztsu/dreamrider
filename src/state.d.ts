f02d66d695772c2e98b94a04403929e6382ad173
Alexey Golovnya <alexey.golovnya@gmail.com>
2018-08-09 22:14:36 +0700

    backup

--- src/state.d.ts
+++ src/state.d.ts
@@ -8,13 +8,25 @@ export interface Result<S> {
     state: S;
 }
 
-export interface Fn<S> {
-    (params: Params<S>): Result<S>;
+export interface ParamsThunk<S> {
+    apply: Apply<S>;
+}
+
+type ResultThunk = void
+
+type Fn<S> =  ((parmas: Params<S>) => Result<S>) | ((params: ParamsThunk<S>) => ResultThunk)
+
+export interface Apply<S> {
+    (...fn: Fn<S>[]): () => void;
+}
+
+export interface Listener<S> {
+    (state: S): void
 }
 
 export interface ConenctedFunctions<S> {
-    apply(...fn: Fn<S>[]): () => void;
-    subscribe(listener: (state: S) => void): void;
+    apply: Apply<S>;
+    subscribe(listener: Listener<S>): void;
     getState(): S;
 }
 
